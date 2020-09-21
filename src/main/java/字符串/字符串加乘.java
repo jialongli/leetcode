650a1ecfda7551ce44d127ac0991244971158c5f
jlli <1986626374@qq.com>
2020-09-21 23:32:45 +0800

    算法

---
+++ src/main/java/字符串/字符串加乘.java
@@ -0,0 +1,48 @@
+package 字符串;
+
+public class 字符串加乘 {
+    public String multiply(String num1, String num2) {
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+
+        int jw = 0;
+        int flag = 0;
+        StringBuilder result = new StringBuilder();
+        while (i >= 0 || j >= 0) {
+            int tmp1 = 0;
+            int tmp2 = 0;
+            if (i >= 0) {
+                // tmp1 = Integer.parseInt(String.valueOf(num1.charAt(flag)));
+                //妙啊  用char减去0的char,那么就是数字了
+                tmp1 = num1.charAt(i) - '0';
+            }
+            if (j >= 0) {
+                tmp2 = num2.charAt(j) - '0';
+            }
+            int sum = tmp1 + tmp2 + jw;
+            jw = sum / 10;
+            result.append(sum % 10);
+            flag++;
+            i--;
+            j--;
+        }
+        if (jw > 0) {
+            result.append(jw);
+        }
+        return result.reverse().toString();
+    }
+
+    private int c(int a,int single){
+
+    }
+
+
+    public static void main(String[] args) {
+        char l = "123".charAt(0);
+        int s = Integer.parseInt(String.valueOf(l));
+        System.out.println(s);
+//
+//        String r = new 字符串加法().addStrings("0", "0");
+//        System.out.println(r);
+    }
+}
